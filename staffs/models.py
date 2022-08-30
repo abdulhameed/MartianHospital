043cc80da08ad5a66810c39ac4ea2177dad5f971
abdulhameed <abdulhameedgiwa@gmail.com>
2022-08-30 17:15:45 +0100

    Initial commit

---
+++ staffs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Appointment model
+
+# class Appointment(models.Model):
+#     staff =
+#     patient =
+#     pendin_approval = models.BooleanField(default=True, null=True)
+#     approved = models.BooleanField(default=False, null=True)
